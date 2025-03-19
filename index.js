4af5d01ea0204d5e635adfba795dc9be35089b4a
Balar Crens <balarcrens@gmail.com>
2025-03-19 17:17:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+const users = {};
+
+io.on('connection', (socket) => {
+    socket.on('name', (name) => {
+        users[socket.id] = name;
+
+        io.emit('name', `${name} has joined the chat.`);
+        io.emit('updateUsers', Object.values(users));
+    });
+    
+    socket.on('msg', (msg) => {
+        socket.broadcast.emit('incoming', { name: users[socket.id], message: msg });
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('name', `${users[socket.id]} has left the chat.`);
+        delete users[socket.id];
+
+        io.emit('updateUsers', Object.values(users));
+    });
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+}); 
+
+server.listen(1234)
