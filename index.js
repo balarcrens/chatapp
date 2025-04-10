c984bb372700b37b19ff5eea5317c40bdb194842
Balar Crens <balarcrens@gmail.com>
2025-04-10 15:07:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const { Server } = require('socket.io');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7777;
 
 const app = express();
 const server = http.createServer(app);
